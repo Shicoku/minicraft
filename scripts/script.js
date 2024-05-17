1eaebba47e1de5bd07b9ca48dcd3fc53311e3c76
Shicoku / Syu <58730464+Shicoku@users.noreply.github.com>
2024-05-17 14:15:13 +0900

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,5 +0,0 @@
-const spacing = '10px';
-const styles = `padding: ${spacing}; background-color: white; color: red; font-style: italic; border: 1px solid black; font-size: 2em;`;
-console.log(`%cStop! このサイトはフリー素材じゃないぞ！
-自分がしていることはわかっているのかね？わかっていないのなら速やかにこの画面を閉じ、なかったことにしよう！
-そうでないなら、お友達になりましょう！`, styles);
